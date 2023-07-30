d370c49b2affce74104f3a161a3ca06673373e75
Tomomi-K1 <tomomi.music@gmail.com>
2023-07-29 18:19:25 -0700

    changed Loader design

--- frontend/src/common/Loader.js
+++ frontend/src/common/Loader.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
+import './Loader.css'
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function Loader(){
     return(
-        <div>
-            <h3>Loading...</h3>
-        </div>
+        <Container sx={{height:'100vh', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+            <Box sx={{display:'flex', justifyContent: "center", width:'25%', alignItems:'center'}}>
+                <i className="fa-solid fa-otter fa-beat-fade"></i>
+                <i className="fa-solid fa-compact-disc"></i>
+                <i className="fa-solid fa-hippo fa-beat-fade"></i>
+                <i className="fa-solid fa-compact-disc"></i>
+                <i className="fa-solid fa-cow fa-beat-fade"></i>
+            </Box>
+                <p class='loading'>Loading...</p>
+        </Container>
     );
 }
 
